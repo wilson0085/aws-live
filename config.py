fa542fff5d8f89e444bf82ec38a996231faa68f7
wilson0085 <138643086+wilson0085@users.noreply.github.com>
2023-08-28 10:48:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.ciljn65lhiji.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wilson-employee"
+customregion = "us-east-1"
+
